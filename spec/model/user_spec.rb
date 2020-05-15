2084a7c09a9eeb47b95f7bed621defde7b542eba
Oscar Mier <voscarmv@gmail.com>
2020-05-15 17:18:14 -0500

    First model test for User.

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,12 @@
+# spec/models/user_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  it "is valid with valid attributes" do
+    expect(User.new(name: "Oscar")).to be_valid
+  end
+  it "is not valid without a name" do
+    expect(User.new).to_not be_valid
+  end
+end
