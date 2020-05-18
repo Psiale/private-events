03b8d5005128412779c4bc229081e8188fa86dab
Oscar Mier <voscarmv@gmail.com>
2020-05-18 17:00:36 -0500

    Finished model tests.

--- spec/model/models_spec.rb
+++ spec/model/models_spec.rb
@@ -48,11 +48,44 @@ RSpec.describe Event, :type => :model do
     t = Event.reflect_on_association(:host)
     expect(t.macro).to eq(:belongs_to)
   end
-  
+  it "should have many event_attendances" do
+    t = Event.reflect_on_association(:event_attendances)
+    expect(t.macro).to eq(:has_many)
+  end
+  it "should have many guests" do
+    t = Event.reflect_on_association(:guests)
+    expect(t.macro).to eq(:has_many)
+  end
 end
 
 RSpec.describe EventAttendance, :type => :model do
-
+  let(:event) { Event.create(location: "Home", time: "2020-05-18 19:46:03") }
+  let(:user) { User.create(name: "Oscar") }
+  let(:user2) { User.create(name: "Alexis") }
+  subject { EventAttendance.new(attended_event: event, guest: user, accepted: nil) }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "is not valid without valid attributes" do
+    subject.attended_event = nil
+    expect(subject).to_not be_valid
+  end
+  it "is valid if the foreign key pair is unique" do
+    EventAttendance.create(attended_event: event, guest: user2, accepted: nil)
+    expect(subject).to be_valid
+  end
+  it "is not valid if the foreign key pair is not unique" do
+    EventAttendance.create(attended_event: event, guest: user, accepted: nil)
+    expect(subject).to_not be_valid
+  end
+  it "should belong to guest" do
+    t = EventAttendance.reflect_on_association(:guest)
+    expect(t.macro).to eq(:belongs_to)
+  end
+  it "should belong to attended_event" do
+    t = EventAttendance.reflect_on_association(:attended_event)
+    expect(t.macro).to eq(:belongs_to)
+  end
 end
 
 # RSpec.describe EventLogic, :type => :model do
