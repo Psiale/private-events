584b196b6222e4000f49de7f172e0e2810c06e29
psialedev <psialedev@gmail.com>
2020-05-19 10:20:37 -0500

    made an integration test for the rooth path

--- spec/features/behavior_spec.rb
+++ spec/features/behavior_spec.rb
@@ -1,2 +1,11 @@
 require 'rails_helper'
 
+RSpec.describe 'User visiting the home page' do
+    scenario "and creating an user" do
+        visit root_path
+        fill_in 'Name', with: 'User test'
+        
+        click_on 'Submit'
+        expect(page).to have_content("User test's Events")
+    end
+end
