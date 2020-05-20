e523115230e684c6c2ec328b9f85b1f91ecb4760
Oscar Mier <voscarmv@gmail.com>
2020-05-20 10:20:58 -0500

    Fixed rspec in prev commit and fixed models here.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,11 +6,6 @@ class Event < ApplicationRecord
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :guests, through: :event_attendances, source: :guest
 
-  # need to fix the relationship alias, this is searching for a hosts table
-  # def  past
-  #     self.where(self.arel_table[:time].lt(DateTime.now))
-  # end
-
   scope :past, -> { where(Event.arel_table[:time].lt(DateTime.now)) }
   scope :upcoming, -> { where(Event.arel_table[:time].gteq(DateTime.now)) }
 end
