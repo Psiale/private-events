6113e084c2807a5d87c48363a74e9232b76c3a2d
Oscar Mier <voscarmv@gmail.com>
2020-05-20 10:28:38 -0500

    Started fixing routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :events
-  # resources :sessions
-  get 'sessions/new'
-  get 'sessions/welcome'
-  post 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
-  resources :event_attendances
+  resources :events, only: [:index, :show, :new, :create]
+  resources :sessions, only: [:new, :welcome, :create, :destroy]
+  # get 'sessions/new'
+  # get 'sessions/welcome'
+  # post 'sessions/create'
+  # get 'sessions/destroy'
+  resources :users, only: [:index, :show, :new, :create, :update]
+  resources :event_attendances, only: [:index, :new, :create, :update]
   root 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
