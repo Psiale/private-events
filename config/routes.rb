3b267a5cde51389f36fa371ad087633177e9dd06
Oscar Mier <voscarmv@gmail.com>
2020-05-20 10:45:03 -0500

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#new'
   resources :events, only: [:index, :show, :new, :create]
-  resources :sessions, only: [:new, :welcome, :create, :destroy]
-  # get 'sessions/new'
-  # get 'sessions/welcome'
-  # post 'sessions/create'
-  # get 'sessions/destroy'
   resources :users, only: [:index, :show, :new, :create, :update]
   resources :event_attendances, only: [:index, :new, :create, :update]
-  root 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  get 'sessions/welcome'
+  post 'sessions/create'
+  get 'sessions/destroy'
 end
